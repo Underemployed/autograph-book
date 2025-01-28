b19d246c4f2c4925e57ed9a057a99ae3c837df35
Underemployed <underemployed871@gmail.com>
2025-01-28 22:36:34 +0530

    works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Hellow from index.js")
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// getting firebase obj get from settings of the app
+import firebaseConfig from "../firebase";
+
+
+// initialize
+const app = initializeApp(firebaseConfig);
+
+
+// init services
+import {
+    getFirestore,
+    collection
+    
+} from "firebase/firestore";
+const db = getFirestore();
+
+// int collection refs
+const collectionRef = collection(db, "books");
+
+console.log("Firebase");
+
+// get collection data
+
+
+
+
+
+
+
+
+
+
+
+
+
+const analytics = getAnalytics(app);
